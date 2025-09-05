391acfbe475a23f6d6f0cf2896ab9044f499d35c
quochuy120124 <quochuy120124@gmail.com>
2025-09-05 02:56:27 +0000

    oke

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage, name=''),
+    path('register', views.RegisterPage, name='register'),
+    path('login', views.LoginPage, name='login'),
+    path('profile', views.ProfilePage, name='profile'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,5 +2,17 @@ from django.http import HttpResponse
 from django.template import loader
 
 def HomePage(request):
-  template = loader.get_template('index.html')
+  template = loader.get_template('HomePage.html')
+  return HttpResponse(template.render())
+
+def LoginPage(request):
+  template = loader.get_template('authentication/Login.html')
+  return HttpResponse(template.render())
+
+def RegisterPage(request):
+  template = loader.get_template('authentication/Register.html')
+  return HttpResponse(template.render())
+
+def ProfilePage(request):
+  template = loader.get_template('Navbar/Profile.html')
   return HttpResponse(template.render())
